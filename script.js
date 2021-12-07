2757ff7d112b4a395ea170d6eeff74c3d0624e7f
Zack Greenfield <zackg007@verizon.net>
2021-12-07 18:59:57 -0500

    added arrays

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Assignment Code
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialcharacters = ['!', '@', '#', '$', '&', '*', '+', '_'];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -13,26 +17,24 @@ function writePassword() {
 
 
 
-const characters = [];
-characters[0] = "lowercase";
-characters[1] = "uppercase";
-characters[2] = "special characters";
+
 
 
 
 
 function generatePassword() {
+  const characters = [];
 
+  characters.push(lowercase[0]);
+  characters.push(uppercase[3]);
+  characters.push(specialcharacters[5]);
+  console.log(characters);
+  
 
-
-  var promptLength = window.prompt('How many characters would you like in your password? Please enter a number between 8 and 128.'); {
-    console.log(promptLength);
-  }
+  var promptLength = window.prompt('How many characters would you like in your password? Please enter a number between 8 and 128.');
  
-  var promptLowercase = window.prompt('Would you like Lowercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".'); {
-    console.log(characters.push(0));
-  }
-  var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var promptLowercase = window.prompt('Would you like Lowercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".');
+  
   var getLowercase = lowercase[Math.floor(Math.random() * lowercase.length)];
   if (promptLowercase === "YES") {
     for (var i = 8; i < (math.ceil(promptLength / 128)); i++) {
@@ -41,10 +43,9 @@ function generatePassword() {
   }
 
 
-  var promptUppercase = window.prompt('Would you like Uppercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".'); {
-    console.log(characters.push(1));
-  }
-  var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var promptUppercase = window.prompt('Would you like Uppercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".');
+
+  
   var getUppercase = uppercase[Math.floor(Math.random() * uppercase.length)];
   if (promptUppercase === "YES") {
     for (var i = 8; i < (math.ceil(promptLength / 128)); i++) {
@@ -52,16 +53,18 @@ function generatePassword() {
     }
   }
 
-  var promptSpecialcharacters = window.prompt('Would you like special characters in your password? Enter "YES or "NO", to choose "yes" or "no".'); {
+  var promptSpecialcharacters = window.prompt('Would you like special characters in your password? Enter "YES or "NO", to choose "yes" or "no".'); 
     console.log(characters.push(2));
-  }
-  var specialcharacters = ['!', '@', '#', '$', '&', '*', '+', '_'];
+  
+  
   var getSpecialcharacters = specialcharacters[Math.floor(Math.random() * specialcharacters.length)];
   if (promptSpecialcharacters === "YES") {
     for (var i = 8; i < (math.ceil(promptLength / 128)); i++) {
       console.log(getSpecialcharacters[i]);
     }
   }
+  // [1,2,3] ==> "123"
+  return characters.join('');
 };
 
 
