aa0bf99bd7b0492831d2be2febc0d4e529e91d5d
Zack Greenfield <zackg007@verizon.net>
2021-12-08 15:04:14 -0500

    added if statements to password generator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialcharacters = ['!', '@', '#', '$', '&', '*', '+', '_'];
 var generateBtn = document.querySelector("#generate");
 
@@ -24,47 +25,43 @@ function writePassword() {
 
 function generatePassword() {
   const characters = [];
+  var password = "";
 
-  characters.push(lowercase[0]);
-  characters.push(uppercase[3]);
-  characters.push(specialcharacters[5]);
-  console.log(characters);
   
 
   var promptLength = window.prompt('How many characters would you like in your password? Please enter a number between 8 and 128.');
- 
-  var promptLowercase = window.prompt('Would you like Lowercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".');
-  
-  var getLowercase = lowercase[Math.floor(Math.random() * lowercase.length)];
-  if (promptLowercase === "YES") {
-    for (var i = 8; i < (math.ceil(promptLength / 128)); i++) {
-      console.log(getLowercase[i]);
-    }
+  if (promptLength < 8 || promptLength > 128) {
+    alert("Please check to make sure the password is between 8 and 128 characters.");
+    return;
   }
 
-
-  var promptUppercase = window.prompt('Would you like Uppercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".');
-
-  
-  var getUppercase = uppercase[Math.floor(Math.random() * uppercase.length)];
-  if (promptUppercase === "YES") {
-    for (var i = 8; i < (math.ceil(promptLength / 128)); i++) {
-      console.log(getUppercase[i]);
-    }
+  var promptLowercase = window.confirm('Would you like Lowercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".');
+  if (promptLowercase == true) {
+    characters.push(...lowercase);
   }
 
-  var promptSpecialcharacters = window.prompt('Would you like special characters in your password? Enter "YES or "NO", to choose "yes" or "no".'); 
-    console.log(characters.push(2));
-  
-  
-  var getSpecialcharacters = specialcharacters[Math.floor(Math.random() * specialcharacters.length)];
-  if (promptSpecialcharacters === "YES") {
-    for (var i = 8; i < (math.ceil(promptLength / 128)); i++) {
-      console.log(getSpecialcharacters[i]);
-    }
+  // My tutor showed me the spread syntax because we could not get concat to work
+  var promptUppercase = window.confirm('Would you like Uppercase letters in your password? Enter "YES" or "NO", to choose "yes" or "no".');
+  if (promptUppercase == true) {
+    characters.push(...uppercase);
   }
-  // [1,2,3] ==> "123"
-  return characters.join('');
+
+  var promptSpecialcharacters = window.confirm('Would you like special characters in your password? Enter "YES" or "NO", to choose "yes" or "no".'); 
+  if (promptSpecialcharacters == true) {
+    characters.push(...specialcharacters);
+  }
+  
+  var promptNumbers = window.confirm('Would you like numbers in your password? Enter "YES" or "NO, to choose "yes" or "no".');
+  if (promptNumbers == true) {
+    characters.push(...numbers);
+  }
+  
+  
+  for (var i = 0; i < promptLength; i++) {
+    var randomNumber = Math.floor(Math.random() * characters.length);
+    password = password + characters [randomNumber];
+  }
+  return password;
 };
 
 
